f102a24ce57f0fcaf9e575d4b9e3cc24ea72b9db
saikamara59 <mhsaidu@gmail.com>
2025-01-29 21:34:19 -0500

    fixed a lil issue

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 def get_db_connection():
     connection = psycopg2.connect(host='localhost',
-                            database='flask_auth_db',
+                            database='thrift_store_db',
                             user=os.getenv('POSTGRES_USERNAME'),
                             password=os.getenv('POSTGRES_PASSWORD'))
     return connection
@@ -49,22 +49,23 @@ def signup():
         new_user_data = request.get_json()
         connection = get_db_connection()
         cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cursor.execute("SELECT * FROM users WHERE username = %s;", (new_user_data["username"],))
+        cursor.execute("SELECT * FROM users WHERE username = %s or email = %s;", (new_user_data["username"], new_user_data["email"],)) 
         existing_user = cursor.fetchone()
         if existing_user:
             cursor.close()
             return jsonify({"error": "Username already taken"}), 400
         hashed_password = bcrypt.hashpw(bytes(new_user_data["password"], 'utf-8'), bcrypt.gensalt())
-        cursor.execute("INSERT INTO users (username,email, password_hash) VALUES (%s, %s, %s) RETURNING id,username, email;", (new_user_data["username"], new_user_data["email"],hashed_password.decode('utf-8')))
+        cursor.execute("INSERT INTO users (username,email, password) VALUES (%s, %s, %s) RETURNING id,username, email;", (new_user_data["username"], new_user_data["email"],hashed_password.decode('utf-8')))
         created_user = cursor.fetchone()
         connection.commit()
         cursor.close()
         connection.close()
-        token = jwt.encode({"id": created_user["id"], "username": created_user["username"]}, 
-                           os.getenv('JWT_SECRET'), algorithm="HS256")
+        payload = {"username": created_user["username"], "id": created_user["id"]}
+        token = jwt.encode({ "payload": payload }, os.getenv('JWT_SECRET'))
         return jsonify({"token": token, "user": created_user}), 201
-    except Exception as error:
-        return jsonify({"error": str(error)}), 401
+    except Exception as err:
+        return jsonify({"err": err}), 401
+
     
 
 @app.route('/auth/signin', methods=["POST"])
@@ -77,8 +78,8 @@ def signin():
         existing_user = cursor.fetchone()
         if existing_user is None:
             return jsonify({"error": "Invalid credentials."}), 401
-        password_is_valid = bcrypt.checkpw(
-            (sign_in_form_data["password"].encode, 'utf-8'), bytes(existing_user["password_hash"].encode, 'utf-8'))
+        password_is_valid = bcrypt.checkpw(bytes(
+            sign_in_form_data["password"], 'utf-8'), bytes(existing_user["password"], 'utf-8'))
         if not password_is_valid:
             return jsonify({"error": "Invalid credentials."}), 401
 
