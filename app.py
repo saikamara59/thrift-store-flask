e11ed5314fba3148aeaa6ad2530e0db7d143dfd7
saikamara59 <mhsaidu@gmail.com>
2025-02-16 18:24:36 -0500

    added heroku route

--- app.py
+++ app.py
@@ -13,10 +13,18 @@ app = Flask(__name__)
 CORS(app)
 
 def get_db_connection():
-    connection = psycopg2.connect(host='localhost',
-                            database='thrift_store_db',
-                            user=os.getenv('POSTGRES_USERNAME'),
-                            password=os.getenv('POSTGRES_PASSWORD'))
+    if 'ON_HEROKU' in os.environ:
+        connection = psycopg2.connect(
+            os.getenv('thrift_store_db'), 
+            sslmode='require'
+        )
+    else: 
+        connection = psycopg2.connect( 
+            host='localhost',
+            database=os.getenv('POSTGRES_DATABASE'),
+            user=os.getenv('POSTGRES_USERNAME'),
+            password=os.getenv('POSTGRES_PASSWORD')
+        )
     return connection
 
 @app.route('/sign-token', methods=['GET'])
